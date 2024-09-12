17c9dbda2a4e169502ca752fbc435830cf1cf24e
Caleb <ritchie.caleb42@gmail.com>
2024-09-12 18:24:55 -0500

    query key practice setup

--- src/App.js
+++ src/App.js
@@ -6,30 +6,22 @@ const POSTS = [
   { id: 2, title: "Post 2"}
 ]
 
+// /posts -> ["posts"]
+// /posts/1 -> ["posts", post.id]
+// /posts?authorId=1 -> ["posts", { authorId: 1 }]
+// /posts/2/comments -> ["posts", post.id, "comments"]
+
 function wait(duration) {
   return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 function App() {
-  const queryClient = useQueryClient()
-  
   console.log("POSTS", POSTS)
 
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS])
   })
-
-  const newPostMutation = useMutation({
-    mutationFn: title => {
-      return wait(1000).then(() => 
-        POSTS.push({ id: crypto.randomUUID(), title })
-    )
-  },
-  onSuccess: () => {
-    queryClient.invalidateQueries(["posts"])
-  }
-})
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>
   if (postsQuery.error) return <pre>{JSON.stringify(postsQuery.error)}</pre>
@@ -39,9 +31,6 @@ return (
     {postsQuery.data.map(post => (
       <div key={post.id}>{post.title}</div>
     ))}
-    <button disabled={newPostMutation.isLoading} onClick={() => newPostMutation.mutate("New Post, Eh")}>
-      Add New 
-    </button>
   </div>
 )
 }
