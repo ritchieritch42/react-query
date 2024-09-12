5c6c1659c4e4983df330a9799adc732add286e97
Caleb <ritchie.caleb42@gmail.com>
2024-09-11 20:45:46 -0500

    new posts query and new post mutation with onSuccess defined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1"},
+  { id: 2, title: "Post 2"}
+]
+
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const queryClient = useQueryClient()
+  
+  console.log("POSTS", POSTS)
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => 
+        POSTS.push({ id: crypto.randomUUID(), title })
+    )
+  },
+  onSuccess: () => {
+    queryClient.invalidateQueries(["posts"])
+  }
+})
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>
+  if (postsQuery.error) return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  
+return (
+  <div>
+    {postsQuery.data.map(post => (
+      <div key={post.id}>{post.title}</div>
+    ))}
+    <button disabled={newPostMutation.isLoading} onClick={() => newPostMutation.mutate("New Post, Eh")}>
+      Add New 
+    </button>
+  </div>
+)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
